14d3d4600e9ef6246f0249811d4ea7e682e00664
Arthur <nashcassano8@gmail.com>
2025-08-19 02:07:08 -0300

    Terminando de implementar meu banco de dados utilizando SQL Server

---
+++ ProjetoFaculdade/Data/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoFaculdade.Data;
+
+public class Context : DbContext
+{
+    public Context(DbContextOptions<Context> options) : base(options)
+    {
+        
+    }
+}
